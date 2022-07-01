6f2586df483ea9f49ce405b05f7a339cc3907dd6
Ri-2020 <rohitgupta111abcd@gmail.com>
2022-07-02 00:24:37 +0530

    local commit

--- lib/screens/dynamic/profile_screen.dart
+++ lib/screens/dynamic/profile_screen.dart
@@ -14,113 +14,36 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // ignore: unused_local_variable
+    double width = size.width;
     return Background(
-      child: SingleChildScrollView(
-        child: Container(
-          margin: const EdgeInsets.only(
-            right: 30,
-            left: 30,
-            top: 70,
-          ),
-          child: Column(
-            children: [
-              Row(
-                children: const [
-                  Text(
-                    "Your Profile",
-                    style: TextStyle(
-                      color: DARK2,
-                      fontSize: HEADINGFONTSIZE2,
-                      fontWeight: FontWeight.bold,
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 40,
+          horizontal: 20,
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: LIGHT2,
+              ),
+              child: Row(children: [
+                CircleAvatar(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                      50,
+                    ),
+                    child: Image.asset(
+                      'assets/images/indian.png',
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                // margin: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: LIGHT2,
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 10,
-                      spreadRadius: -10.0,
-                      offset: Offset(
-                        0.0,
-                        0.0,
-                      ),
-                    )
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(
-                        right: 20,
-                      ),
-                      decoration: const BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 25,
-                            spreadRadius: -15.0,
-                            offset: Offset(
-                              0.0,
-                              0.0,
-                            ),
-                          )
-                        ],
-                      ),
-                      child: CircleAvatar(
-                        radius: 60,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(60),
-                          child: Image.asset("assets/images/indian.png"),
-                        ),
-                      ),
-                    ),
-                    RichText(
-                      // overflow: TextOverflow.ellipsis,
-                      text: const TextSpan(
-                        text: "Samriddhi Singh",
-                        style: TextStyle(
-                          color: DARK2,
-                          // overflow: TextOverflow.fade,
-                          fontWeight: FontWeight.bold,
-                          fontSize: HEADINGFONTSIZE3,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                margin: const EdgeInsets.only(top: 20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: LIGHT2,
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 10,
-                      spreadRadius: -10.0,
-                      offset: Offset(
-                        0.0,
-                        0.0,
-                      ),
-                    )
-                  ],
-                ),
-                child: Row(),
-              ),
-            ],
-          ),
+                )
+              ]),
+            ),
+          ],
         ),
       ),
     );
